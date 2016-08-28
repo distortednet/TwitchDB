e80bfa24cc38cfd39ca42e75c5f5c7dcc342e402
Dimitris Karittevlis <dimitris13k@gmail.com>
2016-08-28 10:20:43 +0100

    parse about text using markdown

--- routes/about.js
+++ routes/about.js
@@ -2,10 +2,14 @@ var express = require('express'),
   config = require('../config'),
   db = require('../db'),
   helpers = require('../helpers'),
-  router = express.Router();
+  router = express.Router(),
+  marked = require('marked');
 
 router.get('/', (req, res, next) => {
   db.intro.selectadmins().then((dbres) => {
+    for (admin of dbres) {
+      admin.intro_data.intro_about = marked(admin.intro_data.intro_about);
+    }
     res.render('about', {admins: dbres, page: 'About'});
   })
 });
